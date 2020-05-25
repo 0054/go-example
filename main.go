e347fe33823673756e0315bed8246cdf86bad610
Евгений И. Пушкин <ei.pushkin@jet.su>
2020-05-25 13:33:08 +0300

    fix logging and hello world message

--- main.go
+++ main.go
@@ -4,17 +4,22 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 )
 
 func logging(f http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		log.Println( r.URL.Path + " - " + r.Method + " - " + " STATUS CODE!!!")
+		log.Println(r.URL.Path + " - " + r.Method)
 		f(w, r)
 	}
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "hello world")
+	hostname, err := os.Hostname()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Fprintf(w, "hello world from "+hostname)
 }
 
 func main() {
